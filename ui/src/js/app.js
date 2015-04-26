6c887ca03c2878aca6d5afbdfeb35908b4f7e708
Chris Wong <chris@cogifire.com>
2015-04-25 19:25:49 -0700

    Replaced codebase with scratch

--- ui/src/js/app.js
+++ ui/src/js/app.js
@@ -6,8 +6,4 @@ angular.module('HackForward', [
 
 .config(function($routeProvider) {
   $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false});
-  $routeProvider.when('/hackathons', {templateUrl:'hackathons.html',  reloadOnSearch: false});
-  $routeProvider.when('/pitches', {templateUrl:'pitches.html',  reloadOnSearch: false});
-  $routeProvider.when('/pitch', {templateUrl:'pitchDetail.html',  reloadOnSearch: false});
-  $routeProvider.when('/live', {templateUrl:'presentation.html',  reloadOnSearch: false});
 });
